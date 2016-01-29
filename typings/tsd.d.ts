5dfcaa76a2a9b6373625429bf9212ce511318ae1
Matthew Skomarovsky <matthew@verypolite.com>
2016-01-29 16:59:48 -0500

    removed installed typings

--- typings/tsd.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="jest/jest.d.ts" />
-/// <reference path="lodash/lodash.d.ts" />
-/// <reference path="node/node.d.ts" />
-/// <reference path="react/react-addons-test-utils.d.ts" />
-/// <reference path="react/react-dom.d.ts" />
-/// <reference path="react/react.d.ts" />
