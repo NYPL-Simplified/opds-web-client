c0ca1eb8af0fcfc0441a1d15fcd244dc5917d371
Matthew Skomarovsky <matthew@verypolite.com>
2016-02-01 11:18:51 -0500

    server port is configurable

--- src/main.tsx
+++ src/main.tsx
@@ -4,22 +4,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Root from './components/Root';
 
-// import parser from 'opds-feed-parser';
-// import * request from 'request';
-
-// request("http://feedbooks.github.io/opds-test-catalog/catalog/acquisition/blocks.xml", (error, response, body) => {
-//   new parser().parse(body).then((feed) => { 
-//     Object.freeze(feed);
-//     console.log(feed);
-
-//     ReactDOM.render(
-//       <Root {...testCollectionData} />,
-//       document.getElementById("opds-browser")
-//     );
-    
-//   });
-// });
-
 import testCollectionData from './testCollectionData';
 
 ReactDOM.render(
