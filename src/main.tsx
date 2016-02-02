d9629341819cbfe5f0306fd73d9e58e215ba6b60
Amy Slagle <amyslagle@nypl.org>
2016-02-02 12:40:58 -0500

    Modified main to use opds-feed-parser. (Fixes #10).

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,28 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Root from './components/Root';
+import OPDSParser = require("opds-feed-parser");
+import { feedToCollection } from "./OPDSDataAdapter";
 
-import testCollectionData from './testCollectionData';
+let parser = new OPDSParser.default();
 
-ReactDOM.render(
-  <Root {...testCollectionData} />,
-  document.getElementById("opds-browser")
-);
+var httpRequest = new XMLHttpRequest();
+httpRequest.onreadystatechange = () => {
+  if (httpRequest.readyState === XMLHttpRequest.DONE) {
+    if (httpRequest.status === 200) {
+      let response = httpRequest.responseText;
+      let promise = parser.parse(response);
+      promise.then((opdsFeed) => {
+        let collectionData = feedToCollection(opdsFeed);
+
+        ReactDOM.render(
+          <Root {...collectionData} />,
+          document.getElementById("opds-browser")
+        );
+      });
+    }
+  }
+}
+httpRequest.open('GET', "http://feedbooks.github.io/opds-test-catalog/catalog/acquisition/main.xml")
+httpRequest.send();
+
--- src/testCollectionData.ts
+++
@@ -1,4 +0,0 @@
-import testOPDSData from './testOPDSData';
-import { feedToCollection } from './OPDSDataAdapter';
-
-export default feedToCollection(testOPDSData);
