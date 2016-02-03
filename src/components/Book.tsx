b8c0f22f3fdf37e3852a930bebaba1f07f835d3a
Amy Slagle <amyslagle@nypl.org>
2016-02-03 17:35:37 -0500

    Cleaned up book styles and classes.

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -6,12 +6,36 @@ export default class Book extends React.Component<any, any> {
   }
 
   render() : JSX.Element {
+    let bookStyle = {
+      float: "left",
+      textAlign: "center",
+      marginRight: "10px",
+      width: "350px",
+      height: "200px",
+      marginBottom: "10px",
+      overflow: "hidden"
+    };
+
+    let bookCoverStyle = {
+      width: "150px",
+      height: "200px",
+      float: "left"
+    };
+
+    let bookInfoStyle = {
+      width: "160px",
+      textAlign: "left",
+      marginTop: "15px",
+      marginLeft: "5px",
+      float: "left"
+    };
+
     return (
-      <div className="opdsEntry" style={{ float: "left", textAlign: "center", marginRight: "10px", width: "350px", height: "200px", marginBottom: "10px", overflow: "hidden" }}>
-        <img src={this.props.imageUrl} style={{ width: "150px", height: "200px", float: "left" }} />
-        <div className="opdsEntryInfo" style={{ float: "left", width: "160px", textAlign: "left", marginTop: "15px", marginLeft: "5px"  }}>
-          <h3 className="opdsEntryTitle">{this.props.title}</h3>
-          <h4 className="opdsEntryAuthors">{this.props.authors.join(", ")}</h4>
+      <div className="book" style={ bookStyle }>
+        <img src={this.props.imageUrl} style={ bookCoverStyle } />
+        <div className="bookInfo" style={ bookInfoStyle }>
+          <h3 className="bookTitle">{this.props.title}</h3>
+          <h4 className="bookAuthors">{this.props.authors.join(", ")}</h4>
         </div>
       </div>
     );
