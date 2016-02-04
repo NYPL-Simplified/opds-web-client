2738a3903162a0c24cd1fdfae399c6d5f14740ac
Matthew Skomarovsky <matthew@verypolite.com>
2016-02-04 17:26:33 -0500

    fixed vertical scroll within lanes

--- src/components/Lane.tsx
+++ src/components/Lane.tsx
@@ -11,7 +11,7 @@ export default class Lane extends React.Component<LaneProps, any> {
       <div className="lane">
         <h2 style={{ clear: "both", paddingTop: "20px" }}>{this.props.title}</h2>
 
-        <div className="laneBooks" style={{ height: "250px", width: "100%", whiteSpace: "nowrap", overflowX: "scroll" }}>
+        <div className="laneBooks" style={{ height: "260px", width: "100%", whiteSpace: "nowrap", overflowX: "scroll", overflowY: "hidden" }}>
           { this.props.books && this.props.books.map(book => 
             <LaneBook key={book.id} {...book} />
           ) }
--- src/main.tsx
+++ src/main.tsx
@@ -9,8 +9,8 @@ let parser = new OPDSParser.default();
 //let feedUrl = "http:\/\/oacontent.alpha.librarysimplified.org/preload";
 //let feedUrl = "http:\/\/oacontent.alpha.librarysimplified.org";
 // let feedUrl = "https:\/\/circulation.librarysimplified.org/feed/eng/English%20-%20Best%20Sellers?order=author";
-// let feedUrl = "https:\/\/circulation.librarysimplified.org";
-let feedUrl = "http:\/\/feedbooks.github.io/opds-test-catalog/catalog/root.xml";
+let feedUrl = "https:\/\/circulation.librarysimplified.org";
+// let feedUrl = "http:\/\/feedbooks.github.io/opds-test-catalog/catalog/root.xml";
 
 var httpRequest = new XMLHttpRequest();
 httpRequest.onreadystatechange = () => {
