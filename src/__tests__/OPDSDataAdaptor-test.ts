17dcb319a6c5f80ea70364d312e3094a5007455a
Amy Slagle <amyslagle@nypl.org>
2016-02-04 15:52:49 -0500

    Cast arguments to avoid having to include things we're not testing.

--- src/__tests__/OPDSDataAdaptor-test.ts
+++ src/__tests__/OPDSDataAdaptor-test.ts
@@ -13,72 +13,41 @@ import { feedToCollection } from '../OPDSDataAdapter';
 
 let artworkUrl = 'https://dlotdqc6pnwqb.cloudfront.net/3M/crrmnr9/cover.jpg';
 
-let artworkLink = new OPDSArtworkLink({
+let artworkLink = new OPDSArtworkLink(<OPDSArtworkLink>{
   href: artworkUrl,
   rel: 'http://opds-spec.org/image',
-  type: '',
-  title: ''
 });
 
-let entry = new OPDSEntry({
+let entry = new OPDSEntry(<OPDSEntry>{
   id: 'urn:librarysimplified.org/terms/id/3M%20ID/crrmnr9',
-  updated: '',
   title: 'The Mayan Secrets',
   authors: [new Contributor({name: 'Clive Cussler', uri: ''}), new Contributor({name: 'Thomas Perry', uri: ''})],
-  contributors: [],
-  categories: [],
-  identifiers: [],
-  issued: '',
-  language: '',
-  rights: '',
-  published: '',
   summary: new Summary({content: '&lt;b&gt;Sam and Remi Fargo race for treasure&#8212;and survival&#8212;in this lightning-paced new adventure from #1&lt;i&gt; New York Times&lt;/i&gt; bestselling author Clive Cussler.&lt;/b&gt;&lt;br /&gt;&lt;br /&gt;Husband-and-wife team Sam and Remi Fargo are in Mexico when they come upon a remarkable discovery&#8212;the mummified remainsof a man clutching an ancient sealed pot. Within the pot is a Mayan book larger than any known before.&lt;br /&gt;&lt;br /&gt;The book contains astonishing information about the Mayans, their cities, and about mankind itself. The secrets are so powerful that some people would do anything to possess them&#8212;as the Fargos are about to find out. Many men and women are going to die for that book.', link: ''}),
-  links: [artworkLink],
-  unparsed: {}
+  links: [artworkLink]
 });
 
-let acquisitionFeed = new AcquisitionFeed({
+let acquisitionFeed = new AcquisitionFeed(<AcquisitionFeed>{
   id: 'feed.xml',
   title: 'Feed',
-  updated: '',
   entries: [entry],
-  links: [],
-  complete: true,
-  unparsed: {}
 });
 
-let navigationLink = new OPDSLink({
+let navigationLink = new OPDSLink(<OPDSLink>{
   rel: 'subsection',
   title: 'title',
   href: 'href',
-  type: ''
 });
 
-let linkEntry = new OPDSEntry({
+let linkEntry = new OPDSEntry(<OPDSEntry>{
   id: 'feed.xml',
-  updated: '',
   title: 'Feed',
-  authors: [],
-  contributors: [],
-  categories: [],
-  identifiers: [],
-  issued: '',
-  language: '',
-  summary: undefined,
-  rights: '',
-  published: '',
   links: [navigationLink],
-  unparsed: {}
 });
 
-let navigationFeed = new NavigationFeed({
+let navigationFeed = new NavigationFeed(<NavigationFeed>{
   id: 'feed.xml',
   title: 'Feed',
-  updated: '',
   entries: [linkEntry],
-  links: [],
-  complete: true,
-  unparsed: {}
 });
 
 
