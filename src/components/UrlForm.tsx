9594ac0bf41a2be6965cab9deb0185db4fd0cc8b
Matthew Skomarovsky <matthew@verypolite.com>
2016-02-05 17:57:19 -0500

    added UrlForm tests

--- src/components/UrlForm.tsx
+++ src/components/UrlForm.tsx
@@ -1,18 +1,25 @@
 import * as React from 'react';
 
 export default class UrlForm extends React.Component<any, any> {
-  render() : JSX.Element {    
+  constructor(props: any) {
+    super(props);
+  }
+
+  render() : JSX.Element {
     return (
       <div id="urlForm" style={{ textAlign: "center", marginTop: "100px" }}>
         <h1>Remote Data URL:</h1>
-        <input ref="input" type="text" style={{ width: "600px", fontSize: "1.2em", padding: "0.5em" }} />&nbsp;
-        <button style={{ fontSize: "1.2em", padding: "0.5em" }} onClick={() => this.onClick()} >Load</button>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input ref="input" type="text" style={{ width: "600px", fontSize: "1.2em", padding: "0.5em" }} />&nbsp;
+          <button type="submit" style={{ fontSize: "1.2em", padding: "0.5em" }}>Load</button>
+        </form>
       </div>
     );
   }
-  
-  onClick() {
+
+  onSubmit(event) {
     let url = this.refs["input"]["value"];
     this.props.fetchUrl(url);
+    event.preventDefault();
   }
 }
