614c956b8ac92d244c0d1b8d3607eb3a47081bb8
Amy Slagle <amyslagle@nypl.org>
2016-02-12 09:42:58 -0500

    Don't scroll to top when there's an error.

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -116,7 +116,7 @@ export default class Collection extends React.Component<CollectionProps, any> {
   }
 
   componentWillReceiveProps(nextProps) {
-    if (this.props.isFetching && !nextProps.isFetching) {
+    if (this.props.isFetching && !nextProps.isFetching && !nextProps.error) {
        document.body.scrollTop = 0;
     }
   }
