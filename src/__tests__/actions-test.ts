72c46659ab7101e7a3bdc5d42448f2c2396639bb
Matthew Skomarovsky <matthew@verypolite.com>
2016-02-16 17:52:29 -0500

    added book action tests

--- src/__tests__/actions-test.ts
+++ src/__tests__/actions-test.ts
@@ -26,6 +26,7 @@ import {
   fetchCollection,
   fetchPage,
   fetchSearchDescription,
+  fetchBook,
   FETCH_COLLECTION_REQUEST,
   FETCH_COLLECTION_SUCCESS,
   FETCH_COLLECTION_FAILURE,
@@ -35,7 +36,10 @@ import {
   FETCH_PAGE_FAILURE,
   LOAD_PAGE,
   LOAD_SEARCH_DESCRIPTION,
-  LOAD_BOOK
+  FETCH_BOOK_REQUEST,
+  FETCH_BOOK_SUCCESS,
+  FETCH_BOOK_FAILURE,
+  LOAD_BOOK,
 } from "../actions";
 
 describe("actions", () => {
@@ -113,4 +117,36 @@ describe("actions", () => {
       }).catch(done);
     });
   });
+
+  describe("fetchBook", () => {
+    let bookUrl = "http://example.com/book";
+
+    it("dispatches request, load, and success", (done) => {
+      let dispatch = jest.genMockFunction();
+      mockFetchData.resolve = true;
+
+      fetchBook(bookUrl)(dispatch).then(data => {
+        expect(dispatch.mock.calls.length).toBe(3);
+        expect(dispatch.mock.calls[0][0].type).toBe(FETCH_BOOK_REQUEST);
+        expect(dispatch.mock.calls[1][0].type).toBe(FETCH_BOOK_SUCCESS);
+        expect(dispatch.mock.calls[2][0].type).toBe(LOAD_BOOK);
+        expect(data).toBe(testData);
+        done();
+      });
+    });
+
+    it("dispatches failure", (done) => {
+      let dispatch = jest.genMockFunction();
+      mockFetchData.resolve = false;
+
+      fetchBook(bookUrl)(dispatch).catch(err => {
+        expect(dispatch.mock.calls.length).toBe(2);
+        expect(dispatch.mock.calls[0][0].type).toBe(FETCH_BOOK_REQUEST);
+        expect(dispatch.mock.calls[1][0].type).toBe(FETCH_BOOK_FAILURE);
+        expect(err).toBe("test error");
+        done();
+      });
+    });
+  });
+
 });
