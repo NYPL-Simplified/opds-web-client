8a4dd6e35bd2f9b59cf2ee45aa57726dd0dfcb24
Amy Slagle <amyslagle@nypl.org>
2016-02-22 17:11:28 -0500

    Reorganized repo and made server a separate npm package.

--- packages/opds-browser/webpack.prod.config.js
+++ packages/opds-browser/webpack.prod.config.js
@@ -7,8 +7,8 @@ var config = {
     ],
   },
   output: {
-    path: './dist',
-    filename: 'opds-browser.js',
+    path: './lib',
+    filename: 'index.js',
     library: 'OPDSBrowser',
     libraryTarget: 'umd'
   },
