5a66aac979271f543a00d29a1a46f8689071161b
Matthew Skomarovsky <matthew@verypolite.com>
2016-03-08 13:26:18 -0500

    rename Root to OPDSBrowser and export it

--- packages/opds-browser/src/app.tsx
+++ packages/opds-browser/src/app.tsx
@@ -2,13 +2,23 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import OPDSBrowser from "./components/OPDSBrowser";
 
+import { createStore, applyMiddleware } from "redux";
+let thunk: any = require("redux-thunk");
+import reducers from "./reducers/index";
+
 class OPDSBrowserApp {
-  browser: OPDSBrowser;
+  browser: any;
 
   constructor(config: any, elementId: string) {
+    let store = createStore(
+      reducers,
+      applyMiddleware(thunk)
+    );
+
     ReactDOM.render(
       <OPDSBrowser
         ref={(c) => this.browser = c}
+        store={store}
         collection={config.collection}
         book={config.book}
         proxyUrl={config.proxyUrl}
@@ -20,7 +30,7 @@ class OPDSBrowserApp {
   }
 
   setCollectionAndBook(collectionUrl: string, bookUrl: string, skipOnNavigate: boolean = false) {
-    this.browser.root.getWrappedInstance().props.setCollectionAndBook(collectionUrl, bookUrl, skipOnNavigate);
+    this.browser.getWrappedInstance().props.setCollectionAndBook(collectionUrl, bookUrl, skipOnNavigate);
   }
 }
 
--- packages/opds-browser/src/components/OPDSBrowser.tsx
+++ packages/opds-browser/src/components/OPDSBrowser.tsx
@@ -1,28 +1,88 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import { connect } from "react-redux";
+import { mapStateToProps, mapDispatchToProps, mergeRootProps } from "./mergeRootProps";
+import Modal from "./Modal";
+import BookDetails from "./BookDetails";
+import LoadingIndicator from "./LoadingIndicator";
+import ErrorMessage from "./ErrorMessage";
+import Collection from "./Collection";
+import UrlForm from "./UrlForm";
+import SkipNavigationLink from "./SkipNavigationLink";
+import { visuallyHiddenStyle } from "./styles";
+
 import { createStore, applyMiddleware } from "redux";
 let thunk: any = require("redux-thunk");
 import reducers from "../reducers/index";
-import Root from "./Root";
 
-export default class OPDSBrowser extends React.Component<RootProps, any> {
-  store: Redux.Store;
-  root: any;
-
-  constructor(props) {
-    super(props);
-    this.store = createStore(
+export class Root extends React.Component<RootProps, any> {
+  static defaultProps = {
+    store: createStore(
       reducers,
       applyMiddleware(thunk)
+    )
+  };
+
+  render(): JSX.Element {
+    let BookDetailsContainer = this.props.BookDetailsContainer;
+
+    return (
+      <div className="browser">
+        <SkipNavigationLink />
+
+        { this.props.isFetching && <LoadingIndicator /> }
+        { this.props.error &&
+          <ErrorMessage
+            message={this.props.error}
+            retry={() => this.props.setCollection(this.props.collectionUrl)} />
+        }
+        { this.props.bookData &&
+            <Modal close={this.props.clearBook}>
+              { BookDetailsContainer ?
+                <BookDetailsContainer book={this.props.bookData}>
+                  <BookDetails book={this.props.bookData} />
+                </BookDetailsContainer> :
+                <BookDetails book={this.props.bookData} />
+              }
+            </Modal>
+        }
+
+        { this.props.collectionData ?
+          <Collection
+            collection={this.props.collectionData}
+            setCollection={this.props.setCollection}
+            fetchPage={this.props.fetchPage}
+            isFetching={this.props.isFetching}
+            isFetchingPage={this.props.isFetchingPage}
+            error={this.props.error}
+            fetchSearchDescription={this.props.fetchSearchDescription}
+            setBook={this.props.setBook}
+            pathFor={this.props.pathFor}
+            history={this.props.history} /> :
+          this.props.isFetching ? null : <UrlForm setCollection={this.props.setCollection} url={this.props.collectionUrl} />
+        }
+      </div>
     );
   }
 
-  render(): JSX.Element {
-    return (
-      <Root
-        ref={c => this.root = c}
-        store={this.store}
-        {...this.props} />
-    );
+  componentWillMount() {
+    if (this.props.collection || this.props.book) {
+      this.props.setCollectionAndBook(this.props.collection, this.props.book, true);
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.collection !== this.props.collection || nextProps.book !== this.props.book) {
+      this.props.setCollectionAndBook(nextProps.collection, nextProps.book);
+    }
   }
 }
+
+let connectOptions = { withRef: true, pure: true };
+const OPDSBrowser = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+  mergeRootProps,
+  connectOptions
+)(Root);
+
+export default OPDSBrowser;
