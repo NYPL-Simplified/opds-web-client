e03b12fb454c50189b66847d50dd009a79f0afd5
Matthew Skomarovsky <matthew@verypolite.com>
2016-03-29 18:04:30 -0400

    fixed mergeRootProps tests

--- packages/opds-browser/src/components/__tests__/mergeRootProps-test.ts
+++ packages/opds-browser/src/components/__tests__/mergeRootProps-test.ts
@@ -7,7 +7,7 @@ import { groupedCollectionData, ungroupedCollectionData } from "./collectionData
 
 describe("mergeRootProps", () => {
   let stateProps, dispatchProps, componentProps;
-  let fetchCollection, clearCollection, fetchBook, loadBook, clearBook, onNavigate;
+  let fetchCollection, clearCollection, fetchBook, loadBook, clearBook, navigate;
   let fakeCollection = ungroupedCollectionData;
   let fakeBook = {
     id: "fake book id",
@@ -29,11 +29,10 @@ describe("mergeRootProps", () => {
     loadBook = jest.genMockFunction();
     clearCollection = jest.genMockFunction();
     clearBook = jest.genMockFunction();
-    onNavigate = jest.genMockFunction();
     dispatchProps = { createDispatchProps: (fetcher) => {
       return { fetchCollection, clearCollection, loadBook, fetchBook, clearBook };
     }};
-    componentProps = { onNavigate };
+    componentProps = {};
   });
 
   describe("setCollection", () => {
@@ -41,9 +40,9 @@ describe("mergeRootProps", () => {
 
     beforeEach(() => {
       stateProps = {
-        collectionUrl: "test url",
+        currentCollectionUrl: "test url",
         collectionData: groupedCollectionData,
-        bookUrl: null,
+        currentBookUrl: null,
         bookData: null
       };
       props = mergeRootProps(stateProps, dispatchProps, componentProps);
@@ -55,9 +54,6 @@ describe("mergeRootProps", () => {
         expect(fetchCollection.mock.calls.length).toBe(1);
         expect(fetchCollection.mock.calls[0][0]).toBe("new collection url");
         expect(clearCollection.mock.calls.length).toBe(0);
-        expect(onNavigate.mock.calls.length).toBe(1);
-        expect(onNavigate.mock.calls[0][0]).toBe("new collection url");
-        expect(onNavigate.mock.calls[0][1]).toBe(stateProps.bookUrl);
         done();
       }).catch(err => done.fail(err));
     });
@@ -67,7 +63,6 @@ describe("mergeRootProps", () => {
         expect(data).toBe(groupedCollectionData);
         expect(fetchCollection.mock.calls.length).toBe(0);
         expect(clearCollection.mock.calls.length).toBe(0);
-        expect(onNavigate.mock.calls.length).toBe(0);
         done();
       }).catch(err => done.fail(err));
     });
@@ -77,22 +72,12 @@ describe("mergeRootProps", () => {
         expect(data).toBeFalsy();
         expect(fetchCollection.mock.calls.length).toBe(0);
         expect(clearCollection.mock.calls.length).toBe(1);
-        expect(onNavigate.mock.calls.length).toBe(1);
-        expect(onNavigate.mock.calls[0][0]).toBe(null);
-        expect(onNavigate.mock.calls[0][1]).toBe(stateProps.bookUrl);
-        done();
-      }).catch(err => done.fail(err));
-    });
-
-    it("does not call onNavigate if given skipOnNavigate = true", (done) => {
-      props.setCollection("test url", true).then(data => {
-        expect(onNavigate.mock.calls.length).toBe(0);
         done();
       }).catch(err => done.fail(err));
     });
 
     it("passes isTopLevel to fetchCollection", (done) => {
-      props.setCollection("new collection url", false, true).then(data => {
+      props.setCollection("new collection url", true).then(data => {
         expect(fetchCollection.mock.calls[0][1]).toBe(true);
         done();
       }).catch(err => done.fail(err));
@@ -104,9 +89,9 @@ describe("mergeRootProps", () => {
 
     beforeEach(() => {
       stateProps = {
-        collectionUrl: "test collection url",
+        currentCollectionUrl: "test collection url",
         collectionData: groupedCollectionData,
-        bookUrl: "test book url",
+        currentBookUrl: "test book url",
         bookData: {
           id: "test book id",
           title: "test book title",
@@ -116,15 +101,21 @@ describe("mergeRootProps", () => {
       props = mergeRootProps(stateProps, dispatchProps, componentProps);
     });
 
-    it("fetches book data if given a book url", (done) => {
+    it("fetches book data if given a book url not in the current collection", (done) => {
       props.setBook("fake book url").then(data => {
         expect(data).toBe(fakeBook);
         expect(fetchBook.mock.calls.length).toBe(1);
         expect(fetchBook.mock.calls[0][0]).toBe("fake book url");
         expect(clearBook.mock.calls.length).toBe(0);
-        expect(onNavigate.mock.calls.length).toBe(1);
-        expect(onNavigate.mock.calls[0][0]).toBe(stateProps.collectionUrl);
-        expect(onNavigate.mock.calls[0][1]).toBe("fake book url");
+        done();
+      }).catch(err => done.fail(err));
+    });
+
+    it("doesn't fetch book data if given a book url in the current collection", (done) => {
+      props.setBook(groupedCollectionData.lanes[0].books[0].url).then(data => {
+        expect(data).toBe(groupedCollectionData.lanes[0].books[0]);
+        expect(fetchBook.mock.calls.length).toBe(0);
+        expect(clearBook.mock.calls.length).toBe(0);
         done();
       }).catch(err => done.fail(err));
     });
@@ -138,17 +129,15 @@ describe("mergeRootProps", () => {
         expect(data).toBe(bookDataWithoutUrl);
         expect(fetchBook.mock.calls.length).toBe(0);
         expect(clearBook.mock.calls.length).toBe(0);
-        expect(onNavigate.mock.calls.length).toBe(1);
         done();
       }).catch(err => done.fail(err));
     });
 
-    it("does nothing and returns existing data if given the existing book url", (done) => {
+    it("tries to refresh book data if given the existing book url", (done) => {
       props.setBook("test book url").then(data => {
-        expect(data).toBe(stateProps.bookData);
-        expect(fetchBook.mock.calls.length).toBe(0);
+        expect(data).toBe(fakeBook);
+        expect(fetchBook.mock.calls.length).toBe(1);
         expect(clearBook.mock.calls.length).toBe(0);
-        expect(onNavigate.mock.calls.length).toBe(0);
         done();
       }).catch(err => done.fail(err));
     });
@@ -158,16 +147,6 @@ describe("mergeRootProps", () => {
         expect(data).toBeFalsy();
         expect(fetchBook.mock.calls.length).toBe(0);
         expect(clearBook.mock.calls.length).toBe(1);
-        expect(onNavigate.mock.calls.length).toBe(1);
-        expect(onNavigate.mock.calls[0][0]).toBe(stateProps.collectionUrl);
-        expect(onNavigate.mock.calls[0][1]).toBe(null);
-        done();
-      }).catch(err => done.fail(err));
-    });
-
-    it("does not call onNavigate if given skipOnNavigate = true", (done) => {
-      props.setBook("test url", true).then(data => {
-        expect(onNavigate.mock.calls.length).toBe(0);
         done();
       }).catch(err => done.fail(err));
     });
