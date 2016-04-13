66d1832f3bb5dec05e95d699e51d04779e8ca019
Matthew Skomarovsky <matthewskomarovsky@nypl.org>
2016-04-13 15:14:19 -0400

    react router handles browser history

--- packages/opds-browser/src/app.tsx
+++ packages/opds-browser/src/app.tsx
@@ -1,22 +1,58 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Router, Route, browserHistory } from 'react-router';
 import OPDSBrowser from "./components/OPDSBrowser";
 import { RootProps } from "./components/Root";
 
+// interface AppProps {
+//   headerTitle?: string;
+//   proxyUrl?: string;
+//   pageTitleTemplate?: (collectionTitle: string, bookTitle: string) => string;
+//   baseCollectionUrl?: string;
+//   baseBookUrl?: string;
+//   navigate?: any;
+// }
+
 class OPDSBrowserApp {
   elementId: string;
-  props: RootProps;
+  BrowserRouteHandler: any;
 
   constructor(config: any, elementId: string) {
     this.elementId = elementId;
-    this.props = config;
-    this.render(config.collectionUrl, config.bookUrl, false);
+
+    this.BrowserRouteHandler = React.createClass({
+      contextTypes: {
+        router: React.PropTypes.object.isRequired
+      },
+      render: function() {
+        let { collectionUrl, bookUrl } = this.props.params;
+        // collectionUrl = collectionUrl ? ((config.baseCollectionUrl || "") + collectionUrl) : "";
+        // bookUrl = bookUrl ? ((config.baseBookUrl || "") + bookUrl) : "";
+        let navigate = (collectionUrl, bookUrl) => {
+          let url = "/";
+          url += collectionUrl ? "collection/" + encodeURIComponent(collectionUrl) : "";
+          url += bookUrl ? "/book/" + encodeURIComponent(bookUrl) : "";
+          console.log("navigating to:", url);
+          this.context.router.push(url);
+        };
+        let mergedProps: RootProps = Object.assign(config, {
+          collectionUrl: collectionUrl ? decodeURIComponent(collectionUrl) : null,
+          bookUrl: bookUrl ? decodeURIComponent(bookUrl) : null,
+          navigate
+        });
+        return <OPDSBrowser {...mergedProps} />;
+      }
+    });
+
+    this.render();
   }
 
-  render(collectionUrl: string = null, bookUrl: string = null, isTopLevel: boolean = false) {
-    let props = Object.assign({}, this.props, { collectionUrl, bookUrl, isTopLevel });
+  render() {
+    let BrowserRouteHandler = this.BrowserRouteHandler;
     ReactDOM.render(
-      <OPDSBrowser {...props} />,
+      <Router history={browserHistory}>
+        <Route path="/(collection/:collectionUrl)(/)(book/:bookUrl)" component={BrowserRouteHandler} />
+      </Router>,
       document.getElementById(this.elementId)
     );
   }
