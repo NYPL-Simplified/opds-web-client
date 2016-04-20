4dbb56307248a9bcf56666ed6d0487c8c8ecd0d5
Matthew Skomarovsky <matthewskomarovsky@nypl.org>
2016-04-20 12:42:47 -0400

    using enzyme in ErrorMessage tests

--- packages/opds-browser/src/components/__tests__/ErrorMessage-test.tsx
+++ packages/opds-browser/src/components/__tests__/ErrorMessage-test.tsx
@@ -1,51 +1,50 @@
-jest.dontMock("../ErrorMessage");
+jest.autoMockOff();
 
 import * as React from "react";
-import * as ReactDOM from "react-dom";
-import * as TestUtils from "react-addons-test-utils";
+import { shallow } from "enzyme";
 
 import ErrorMessage from "../ErrorMessage";
 
 describe("ErrorMessage", () => {
   it("shows the message", () => {
-    let error = TestUtils.renderIntoDocument(
+    let wrapper = shallow(
       <ErrorMessage message="test error" />
-    ) as ErrorMessage;
+    );
 
-    let element = ReactDOM.findDOMNode(error);
-    expect(element.textContent).toContain("test error");
+    let message = wrapper.find(".errorMessage");
+    expect(message.text()).toBe("test error");
   });
 
   it("goes back", () => {
-    let error = TestUtils.renderIntoDocument(
+    let wrapper = shallow(
       <ErrorMessage message="test error" />
-    ) as ErrorMessage;
+    );
 
-    let button = TestUtils.findRenderedDOMComponentWithClass(error, "errorCloseButton");
+    let button = wrapper.find(".errorCloseButton");
     spyOn(window.history, "back");
-    TestUtils.Simulate.click(button);
+    button.simulate("click");
 
     expect(window.history.back).toHaveBeenCalled();
   });
 
   it("retries", () => {
     let retry = jest.genMockFunction();
-    let error = TestUtils.renderIntoDocument(
+    let wrapper = shallow(
       <ErrorMessage message="test error" retry={retry} />
-    ) as ErrorMessage;
+    );
 
-    let button = TestUtils.findRenderedDOMComponentWithClass(error, "retryButton");
-    TestUtils.Simulate.click(button);
+    let button = wrapper.find(".retryButton");
+    button.simulate("click");
 
     expect(retry.mock.calls.length).toBe(1);
   });
 
   it("uses bootstrap classes", () => {
-    let error = TestUtils.renderIntoDocument(
+    let wrapper = shallow(
       <ErrorMessage message="test error" />
-    ) as ErrorMessage;
+    );
 
-    let buttons = TestUtils.scryRenderedDOMComponentsWithClass(error, "btn");
+    let buttons = wrapper.find(".btn");
     expect(buttons.length).toBe(2);
   });
 });
