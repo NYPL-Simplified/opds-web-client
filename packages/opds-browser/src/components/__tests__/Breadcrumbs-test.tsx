a81397f77f000cedb18e3a362c56a84e283b8971
Matthew Skomarovsky <matthewskomarovsky@nypl.org>
2016-05-11 15:11:57 -0400

    added tests for hierarchyComputeBreadcrumbs

--- packages/opds-browser/src/components/__tests__/Breadcrumbs-test.tsx
+++ packages/opds-browser/src/components/__tests__/Breadcrumbs-test.tsx
@@ -3,7 +3,7 @@ jest.autoMockOff();
 import * as React from "react";
 import { shallow } from "enzyme";
 
-import Breadcrumbs from "../Breadcrumbs";
+import Breadcrumbs, { hierarchyComputeBreadcrumbs } from "../Breadcrumbs";
 import BrowserLink, { BrowserLinkProps } from "../BrowserLink";
 import { ungroupedCollectionData } from "./collectionData";
 import { LinkData } from "../../interfaces";
@@ -37,5 +37,64 @@ describe("Breadcrumbs", () => {
     // last link is wrapped in <strong>
     expect((links.at(1).props().children as any).props.children).toContain("last title");
     expect(links.at(1).props().collectionUrl).toEqual("last url");
+  });
+});
+
+describe("hierarchyComputeBreadcrumbs", () => {
+  let collection = {
+    id: "new id",
+    url: "new url",
+    title: "new title",
+    lanes: [],
+    books: [],
+    links: []
+  };
+  let history = [];
+
+  it("returns empty without root or uplink", () => {
+    expect(hierarchyComputeBreadcrumbs(collection, history)).toEqual([]);
+  });
+
+  it("returns root if only root is present", () => {
+    let catalogRootLink = {
+      url: "new root url",
+      text: "new root url"
+    };
+    let data = Object.assign({}, collection, { catalogRootLink });
+    expect(hierarchyComputeBreadcrumbs(data, history)).toEqual([catalogRootLink]);
+  });
+
+  it("provides default catalog root title", () => {
+    let catalogRootLink = {
+      url: "new root url",
+      text: null
+    };
+    let data = Object.assign({}, collection, { catalogRootLink });
+    expect(hierarchyComputeBreadcrumbs(data, history)).toEqual([{
+      url: catalogRootLink.url,
+      text: "Catalog"
+    }]);
+  });
+
+  it("returns uplink if only uplink is present", () => {
+    let parentLink = {
+      url: "new parent url",
+      text: "new parent text"
+    };
+    let data = Object.assign({}, collection, { parentLink });
+    expect(hierarchyComputeBreadcrumbs(data, history)).toEqual([parentLink]);
+  });
+
+  it("returns only root if uplink is same as root", () => {
+    let catalogRootLink = {
+      url: "new root url",
+      text: "new root text"
+    };
+    let parentLink = {
+      url: "new root url",
+      text: "new root text"
+    };
+    let data = Object.assign({}, collection, { catalogRootLink, parentLink });
+    expect(hierarchyComputeBreadcrumbs(data, history)).toEqual([catalogRootLink]);
   });
 });
