c97218b242dcfa78b7f5079a9e4795001b373061
Matthew Skomarovsky <matthewskomarovsky@nypl.org>
2016-06-07 10:51:21 -0400

    updated dependencies

--- packages/opds-web-client/src/store.ts
+++ packages/opds-web-client/src/store.ts
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
-import * as thunk from "redux-thunk";
 import reducers from "./reducers/index";
+const thunk = require("redux-thunk").default;
 
 export default function buildStore(initialState?: any) {
   return createStore(
