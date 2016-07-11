1da12edf615b4f390dad03dc17a7ab14b7060f70
Matthew Skomarovsky <matthewskomarovsky@nypl.org>
2016-07-11 16:29:53 -0400

    added state test

--- packages/opds-web-client/src/__tests__/state-test.ts
+++ packages/opds-web-client/src/__tests__/state-test.ts
@@ -7,8 +7,11 @@ let fetchCollectionAndBook;
 let createFetchCollectionAndBook = jest.genMockFunction();
 createFetchCollectionAndBook.mockImplementation((collectionUrl, bookUrl) => fetchCollectionAndBook);
 let dispatch = () => {};
+let initialState = "initial state";
+let alteredState = "state with collection and book";
+let testState = initialState;
 jest.setMock("../components/mergeRootProps", { createFetchCollectionAndBook });
-jest.setMock("../store", { default: () => { return { dispatch }; } });
+jest.setMock("../store", { default: () => { return { dispatch, getState: () => testState }; } });
 
 import buildInitialState from "../state";
 
@@ -19,18 +22,20 @@ describe("buildInitialState", () => {
   beforeEach(() => {
     fetchCollectionAndBook = jest.genMockFunction();
     fetchCollectionAndBook.mockReturnValue(new Promise((resolve, reject) => {
+      testState = alteredState;
       resolve({ collectionData: null, bookData: null });
     }));
     createFetchCollectionAndBook.mockClear();
   });
 
-  it("fetches given collection and book", (done) => {
+  it("fetches given collection and book into state", (done) => {
     buildInitialState(collectionUrl, bookUrl).then(state => {
       expect(createFetchCollectionAndBook.mock.calls.length).toBe(1);
       expect(createFetchCollectionAndBook.mock.calls[0][0]).toBe(dispatch);
       expect(fetchCollectionAndBook.mock.calls.length).toBe(1);
       expect(fetchCollectionAndBook.mock.calls[0][0]).toBe(collectionUrl);
       expect(fetchCollectionAndBook.mock.calls[0][1]).toBe(bookUrl);
+      expect(state).toBe(alteredState);
       done();
     }).catch(err => done(err));
   });
