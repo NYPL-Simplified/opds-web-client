4046a242ddafc744cb558e9b1b9452f793494fb1
Amy Slagle <amyslagle@nypl.org>
2016-09-01 10:00:36 -0400

    Moved BookCover component styling to scss.

--- packages/opds-web-client/src/components/BookCover.tsx
+++ packages/opds-web-client/src/components/BookCover.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import "../stylesheets/book_cover.scss";
 import { BookData } from "../interfaces";
 const seedrandom = require("seedrandom");
 
@@ -25,19 +26,12 @@ export default class BookCover extends React.Component<BookCoverProps, any> {
   }
 
   render() {
-    let bookCoverStyle = {
-      width: "150px",
-      height: "200px",
-      float: "left",
-      border: "1px solid #ccc"
-    };
-
     if (this.props.book.imageUrl && !this.state.error) {
       return (
         <img
           src={this.props.book.imageUrl}
           onError={this.handleError}
-          style={bookCoverStyle}
+          className="book-cover"
           alt=""
           />
       );
@@ -45,58 +39,29 @@ export default class BookCover extends React.Component<BookCoverProps, any> {
 
     let { title, authors } = this.props.book;
 
-    let titleStyle = Object.assign({
-      display: "table-cell",
-      height: "120px",
-      width: "150px",
-      verticalAlign: "bottom",
-      color: "#444",
-      backgroundColor: "#eee",
-      fontWeight: "normal",
-      padding: "10px",
-    }, this.computeFontStyle(title, 40));
-
-    let authorStyle = Object.assign({
-      paddingTop: "10px",
-      color: "#fff",
-      padding: "10px"
-    }, this.computeFontStyle(authors.join(", "), 25));
+    let titleFontSize = this.computeFontSize(title, 40);
+    let authorFontSize = this.computeFontSize(authors.join(", "), 25);
 
     let hue = this.seededRandomHue(title);
     let bgColor = `hsla(${hue}, 40%, 60%, 1)`;
-    let cellStyle = {
-      float: "left",
-      width: "150px",
-      height: "200px",
-      backgroundColor: bgColor,
-      border: "1px solid #888",
-      fontWeight: "normal",
-      fontFamily: "Georgia, serif",
-      position: "relative",
-      textAlign: "left"
-    };
 
     return (
-      <div className="autoBookCover" style={cellStyle}>
-        <div style={titleStyle}>{ title }</div>
-        <div style={authorStyle}>{ authors.join(", ") }</div>
+      <div className="auto-book-cover" style={{ backgroundColor: bgColor }}>
+        <div className="title" style={{ fontSize: titleFontSize }}>{ title }</div>
+        <div className="authors" style={{ fontSize: authorFontSize }}>{ authors.join(", ") }</div>
       </div>
     );
   }
 
-  computeFontStyle(text, baseFontSize = 40, minFontSize = 15) {
+  computeFontSize(text, baseFontSize = 40, minFontSize = 15) {
     // decrease size as max word length increases
     // decrease size as word count grows beyond 3
     let words = text.split(/\s/);
     let wordCount = words.length;
     let maxLength = Math.max(...words.map(word => word.length));
     let fontSize = Math.max(minFontSize, baseFontSize - maxLength * 2 - Math.max(0, wordCount - 3) * 2);
-    let lineHeight = fontSize + 5;
 
-    return {
-      fontSize: (fontSize / 13) + "em",
-      lineHeight: "1em"
-    };
+    return (fontSize / 13) + "em";
   }
 
   seededRandomHue(seed) {
