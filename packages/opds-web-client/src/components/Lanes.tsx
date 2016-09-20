183773b75479414ea4ab03add9daaea0a1e805ce
Amy Slagle <amyslagle@nypl.org>
2016-09-20 17:28:30 -0400

    Made lane key more unique so scroll position won't get shared between different lanes.

--- packages/opds-web-client/src/components/Lanes.tsx
+++ packages/opds-web-client/src/components/Lanes.tsx
@@ -38,7 +38,7 @@ export class Lanes extends React.Component<any, any> {
         { this.props.lanes && this.props.lanes.length > 0 ?
           <ul aria-label="groups of books" className="subtle-list">
           { this.props.lanes && this.props.lanes.map(lane =>
-            <li key={lane.title}>
+            <li key={lane.url}>
               <Lane
                 lane={lane}
                 collectionUrl={this.props.url}
