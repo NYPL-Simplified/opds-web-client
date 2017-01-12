3d2778a8337d5078213b7b926d6d509278d35c21
Amy Slagle <amyslagle@nypl.org>
2017-01-12 16:25:04 -0500

    Moved skip navigation link tab index to containing element.

--- packages/opds-web-client/src/components/SkipNavigationLink.tsx
+++ packages/opds-web-client/src/components/SkipNavigationLink.tsx
@@ -4,8 +4,8 @@ export default class SkipNavigationLink extends React.Component<any, any> {
   render(): JSX.Element {
     let tabIndex = -1;
     return(
-      <div className="skip-navigation">
-        <a href={this.props.target} tabIndex={tabIndex}>Skip Navigation</a>
+      <div className="skip-navigation" tabIndex={tabIndex}>
+        <a href={this.props.target}>Skip Navigation</a>
       </div>
     );
   }
