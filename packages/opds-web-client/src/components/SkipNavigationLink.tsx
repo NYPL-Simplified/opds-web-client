baf5eeac59af35dec34b4598e190eb9126cb4089
Amy Slagle <amyslagle@nypl.org>
2017-01-12 12:20:04 -0500

    Fixed tab index of skip navigation links.

--- packages/opds-web-client/src/components/SkipNavigationLink.tsx
+++ packages/opds-web-client/src/components/SkipNavigationLink.tsx
@@ -2,9 +2,10 @@ import * as React from "react";
 
 export default class SkipNavigationLink extends React.Component<any, any> {
   render(): JSX.Element {
+    let tabIndex = -1;
     return(
       <div className="skip-navigation">
-        <a href={this.props.target} tabindex="-1">Skip Navigation</a>
+        <a href={this.props.target} tabIndex={tabIndex}>Skip Navigation</a>
       </div>
     );
   }
