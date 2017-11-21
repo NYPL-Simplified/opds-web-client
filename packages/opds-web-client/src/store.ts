7d951b87539e22c85479669541cf1aaabe5bbac6
Amy Slagle <amyslagle@nypl.org>
2017-11-21 17:18:54 -0500

    Don't load redux-localstorage unless localStorage is available.

--- packages/opds-web-client/src/store.ts
+++ packages/opds-web-client/src/store.ts
@@ -6,27 +6,37 @@ const thunk = require("redux-thunk").default;
 import createAuthMiddleware from "./authMiddleware";
 import AuthPlugin from "./AuthPlugin";
 import { PathFor } from "./interfaces";
-const persistState = require("redux-localstorage");
+let persistState = null;
+try {
+  const testKey = String(Math.random());
+  window.localStorage.setItem(testKey, "test");
+  window.localStorage.removeItem(testKey);
+  persistState = require("redux-localstorage");
+} catch (e) {
+  // localStorage isn't available in this environment, so preferences won't be saved.
+}
 
 export default function buildStore(initialState?: State, authPlugins?: AuthPlugin[], pathFor?: PathFor): Store<State> {
   const middlewares = authPlugins && authPlugins.length ? [createAuthMiddleware(authPlugins, pathFor), thunk] : [thunk];
+  const composeArgs = [applyMiddleware(...middlewares)];
+  if (persistState) {
+    composeArgs.push(persistState("preferences"));
+  }
   return createStore<State>(
     reducers,
     initialState,
-    compose<any, any>(
-      applyMiddleware(...middlewares),
-      persistState("preferences")
-    )
+    compose.apply(this, composeArgs)
   );
 }
 
 export function buildCollectionStore(initialState?): Store<any> {
+  const composeArgs = [applyMiddleware(thunk)];
+  if (persistState) {
+    composeArgs.push(persistState("preferences"));
+  }
   return createStore<any>(
     combineReducers({ collection }),
     initialState,
-    compose(
-      applyMiddleware(thunk),
-      persistState("preferences")
-    )
+    compose.apply(this, composeArgs)
   );
 }
