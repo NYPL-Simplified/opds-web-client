7c32f61a1397ee0132454e5b71b1afcb572a657d
Edwin Guzman <edwin.gzmn@gmail.com>
2019-04-11 14:28:45 -0400

    Updating webpack config for dev and prod.

--- packages/opds-web-client/webpack.prod.config.js
+++ packages/opds-web-client/webpack.prod.config.js
@@ -1,32 +1,41 @@
 var webpack = require('webpack');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const merge = require('webpack-merge');
+const path = require("path");
 
-var config = {
+const common = require('./webpack.common.js');
+
+var config = merge(common, {
+  mode: "production",
+  // devtool: "source-map",
   entry: {
     app: [
       './src/app.tsx',
     ],
   },
   output: {
-    path: './dist',
+    path: path.resolve(__dirname, "./dist"),
     filename: 'opds-web-client.js',
     library: 'OPDSWebClient',
     libraryTarget: 'umd'
   },
   plugins: [
-    new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV) }),
     // jsdom is needed for server rendering, but causes errors
     // in the browser even if it is never used, so we ignore it:
     new webpack.IgnorePlugin(/jsdom$/),
 
     // Extract separate css file.
-    new ExtractTextPlugin("opds-web-client.css")
+    new MiniCssExtractPlugin({ filename: "opds-web-client.css" })
   ],
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
       },
       {
         test: /\.tsx?$/,
@@ -34,20 +43,9 @@ var config = {
         loaders: [
           'ts-loader'
         ]
-      },
-      {
-        test: /\.json$/,
-        loaders: ['json-loader']
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg).*$/,
-        loader: 'url-loader?limit=100000'
       }
     ],
-  },
-  resolve: {
-    extensions: ["", ".webpack.js", ".web.js", ".js", ".ts", ".tsx", ".scss"]
   }
-};
+});
 
 module.exports = config;
