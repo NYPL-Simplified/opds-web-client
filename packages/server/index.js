1f1a1042a9f3df7332d684bbd9a9b6817650ca76
Edwin Guzman <edwin.gzmn@gmail.com>
2019-10-18 12:35:56 -0400

    Adding the nypl-simplified-webpub-viewer back and updating how it's rendered for a poc.

--- packages/server/index.js
+++ packages/server/index.js
@@ -6,51 +6,18 @@ var port = process.env.PORT || 3000;
 var multer  = require('multer');
 var form = multer();
 
-var serverManifestJson = require("r2-streamer-js/dist/src/server-manifestjson").serverManifestJson;
-var serverMediaOverlays = require("r2-streamer-js/dist/src/server-mediaoverlays").serverMediaOverlays;
-var serverPub = require("r2-streamer-js/dist/src/server-pub").serverPub;
-var serverAssets = require("r2-streamer-js/dist/src/server-assets").serverAssets;
-var Server = require("r2-streamer-js/dist/src/server").Server
-
 app.use(express.static(__dirname + "/../opds-web-client/dist"));
-// Uncomment to enable the webreader when `nypl-simplified-webpub-viewer` is installed.
-// app.use(express.static(__dirname + "/node_modules/nypl-simplified-webpub-viewer/dist"));
-app.use(express.static(__dirname + "/node_modules/whatwg-fetch"));
-app.use(express.static(__dirname + "/node_modules/promise-polyfill"));
-app.use(express.static(__dirname + "/node_modules/requirejs"));
-app.use(express.static(__dirname + "/node_modules/url-polyfill"));
+// The following files are needed for /reader which calls /viewer for
+// the required files.
+app.use("/viewer", express.static(__dirname + "/node_modules/nypl-simplified-webpub-viewer/dist"));
+// The /dist folder for the webpub viewer does not contain requirejs.
+app.use("/viewer", express.static(__dirname + "/node_modules/requirejs"));
 app.set('views', __dirname + "/views");
 app.set('view engine', 'ejs');
 
 app.listen(port, function() {
   console.log("Server listening on port " + port);
 });
-
-class RemoteEpubServer {
-  constructor() {
-    this.publications = [];
-    this.pathPublicationMap = {};
-    var router = serverPub(this, app);
-    serverManifestJson(this, router);
-    serverAssets(this, router);
-    serverMediaOverlays(this, router);
-  }
-
-  addPublications(pubs) {
-    Server.prototype.addPublications.call(this, pubs);
-  }
-  isPublicationCached(filePath) {
-    Server.prototype.isPublicationCached.call(this, filePath);
-  }
-  cachedPublication(filePath) {
-    Server.prototype.cachedPublication.call(this, filePath);
-  }
-  cachePublication(filePath, pub) {
-    Server.prototype.cachePublication.call(this, filePath, pub);
-  }
-}
-
-var streamer = new RemoteEpubServer();
 
 app.post("/proxy", form.array(), function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
