5ade76978abc150cdd76ba1a5d6b8e5c9a5c964c
kristojorg <kristo.jorgenson@gmail.com>
2020-02-14 10:55:15 +0100

    fix failing test

--- packages/opds-web-client/src/components/__tests__/BasicAuthForm-test.tsx
+++ packages/opds-web-client/src/components/__tests__/BasicAuthForm-test.tsx
@@ -6,6 +6,7 @@ import { shallow, mount } from "enzyme";
 
 import BasicAuthForm from "../BasicAuthForm";
 import BasicAuthPlugin from "../../BasicAuthPlugin";
+import { generateCredentials } from "../../utils/auth";
 
 describe("BasicAuthForm", () => {
   describe("rendering", () => {
@@ -135,9 +136,7 @@ describe("BasicAuthForm", () => {
         username.value = "doubleohseven";
         password = wrapper.find("input[type='password']").getDOMNode();
         password.value = "thenameisbond";
-        credentials = wrapper
-          .instance()
-          .generateCredentials("doubleohseven", "thenameisbond");
+        credentials = generateCredentials("doubleohseven", "thenameisbond");
         validate = stub().returns(true);
         wrapper.instance().validate = validate;
         form = wrapper.find("form");
