0a728722117d591247b2b2c1c80ab66dbb02e03f
Kristo Jorgenson <kristo.jorgenson@gmail.com>
2020-07-10 14:39:45 +0200

    update documentation link

--- packages/opds-web-client/src/interfaces.ts
+++ packages/opds-web-client/src/interfaces.ts
@@ -1,7 +1,7 @@
 import AuthPlugin from "./AuthPlugin";
 
 // the source of truth for media types is located at:
-// https://github.com/NYPL-Simplified/server_core/blob/master/model/constants.py#L237
+// https://github.com/NYPL-Simplified/server_core/blob/master/model/constants.py
 export type MediaType =
   | "application/epub+zip"
   | "application/kepub+zip"
